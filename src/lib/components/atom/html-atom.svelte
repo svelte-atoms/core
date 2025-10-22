1c7b5e84c15dd58c892a82ca1b9a762947c1bfec
abdel <rioman19@gmail.com>
2025-10-23 06:43:05 +0800

    feat: handle presets and introduce `$preset` placeholder for better customization

--- src/lib/components/atom/html-atom.svelte
+++ src/lib/components/atom/html-atom.svelte
@@ -1,9 +1,11 @@
 <script lang="ts" generics="E extends keyof HTMLElementTagNameMap = 'div', B extends Base = Base">
 	import type { HTMLAttributes } from 'svelte/elements';
-	import type { ComponentBase, HtmlAtomProps, SnippetBase } from './types';
+	import type { Base, ComponentBase, HtmlAtomProps, SnippetBase } from './types';
 	import { RootBond } from '../root';
 	import { HtmlElement } from '../element';
-	import { cn } from '$svelte-atoms/core/utils';
+	import { cn, toClassValue } from '$svelte-atoms/core/utils';
+	import { getPreset } from '$svelte-atoms/core/context';
+	import type { PresetModuleName } from '$svelte-atoms/core/context/preset.svelte';
 
 	type Element = HTMLElementTagNameMap[E];
 
@@ -13,13 +15,44 @@
 		class: klass = '',
 		as = 'div',
 		base = undefined,
+		preset: presetKey = undefined,
+		bond = undefined,
 		children = undefined,
 		...restProps
 	}: HtmlAtomProps<E, B> & HTMLAttributes<Element> = $props();
 
-	const isSnippet = $derived(typeof base === 'function' && base.length === 1 && !base.prototype);
+	const preset = $derived(presetKey ? getPreset(presetKey as PresetModuleName) : undefined);
 
-	const snippet = $derived(base as SnippetBase);
+	const _klass = $derived.by(() => {
+		const klasses = Array.isArray(klass) ? klass : [klass];
+
+		const hasPresetPlaceholder = klasses.some(
+			(cls) => typeof cls === 'string' && cls.includes('$preset')
+		);
+
+		if (hasPresetPlaceholder) {
+			return klasses.map((cls) => {
+				if (typeof cls === 'string' && cls.includes('$preset')) {
+					return cls.replace('$preset', cn(toClassValue.apply(bond, [preset?.class])));
+				}
+
+				return toClassValue.apply(bond, [cls]);
+			});
+		}
+
+		return [preset?.class ?? '', '$preset', klass].map((arg) => toClassValue.apply(bond, [arg]));
+	});
+
+	const _base = $derived(base ?? preset?.base);
+	const _as = $derived(as ?? preset?.as);
+	const _restProps = $derived.by(() => {
+		const { class: klass, base, as, ...restPresetProps } = preset ?? {};
+		return { ...restPresetProps, ...restProps };
+	});
+
+	const isSnippet = $derived(typeof _base === 'function' && _base.length === 1 && !_base.prototype);
+
+	const snippet = $derived(_base as SnippetBase);
 
 	const atom = rootBond?.state?.props?.renderers?.html ?? HtmlElement;
 
@@ -27,9 +60,9 @@
 </script>
 
 {#if isSnippet}
-	{@render snippet({ class: cn(klass), as, base, children, ...restProps })}
+	{@render snippet({ class: _klass, as: _as, base: _base, children, ..._restProps })}
 {:else}
-	<Component class={cn(klass)} {as} {...restProps}>
+	<Component class={_klass} as={_as} {..._restProps}>
 		{@render children?.()}
 	</Component>
 {/if}
