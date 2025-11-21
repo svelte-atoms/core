2af36302f9ff54ab833997dd8657dff7a6a79f73
abdel <rioman19@gmail.com>
2025-11-21 11:17:02 +0100

    fix: remove HtmlAtom wrapper from sidebar-root component - layout will be handleded externally by the developer

--- src/lib/components/sidebar/sidebar-root.svelte
+++ src/lib/components/sidebar/sidebar-root.svelte
@@ -1,22 +1,14 @@
 <script lang="ts" generics="E extends keyof HTMLElementTagNameMap = 'div', B extends Base = Base">
 	import { defineProperty, defineState } from '$svelte-atoms/core/utils';
-	import { HtmlAtom, type Base } from '$svelte-atoms/core/components/atom';
+	import { type Base } from '$svelte-atoms/core/components/atom';
 	import { SidebarBond, SidebarBondState, type SidebarBondProps } from './bond.svelte';
 	import type { SlideoverRootProps } from './types';
 
 	let {
 		open = $bindable(false),
-		class: klass = '',
 		disabled = false,
 		factory = _factory,
-		children = undefined,
-		onmount = undefined,
-		ondestroy = undefined,
-		animate = undefined,
-		enter = undefined,
-		exit = undefined,
-		initial = undefined,
-		...restProps
+		children = undefined
 	}: SlideoverRootProps<E, B> = $props();
 
 	const bondProps = defineState<SidebarBondProps>(
@@ -35,11 +27,6 @@
 
 	const bond = factory(bondProps).share();
 
-	const rootProps = $derived({
-		...bond.root(),
-		...restProps
-	});
-
 	function _factory(props: typeof bondProps) {
 		const bondState = new SidebarBondState(() => props);
 		const bond = new SidebarBond(bondState);
@@ -52,17 +39,4 @@
 	}
 </script>
 
-<HtmlAtom
-	{bond}
-	preset="sidebar"
-	class={['border-border flex min-h-full w-full flex-1 flex-nowrap', '$preset', klass]}
-	enter={enter?.bind(bond.state)}
-	exit={exit?.bind(bond.state)}
-	initial={initial?.bind(bond.state)}
-	animate={animate?.bind(bond.state)}
-	onmount={onmount?.bind(bond.state)}
-	ondestroy={ondestroy?.bind(bond.state)}
-	{...rootProps}
->
-	{@render children?.({ sidebar: bond })}
-</HtmlAtom>
+{@render children?.({ sidebar: bond })}
