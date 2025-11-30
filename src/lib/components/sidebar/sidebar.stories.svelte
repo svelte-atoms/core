960ab72bf582ae38585399093fb0d5925d471d59
abdel <rioman19@gmail.com>
2025-11-30 23:43:59 +0100

    fix: simplify sidebar story structure and remove unused imports for improved clarity

--- src/lib/components/sidebar/sidebar.stories.svelte
+++ src/lib/components/sidebar/sidebar.stories.svelte
@@ -1,11 +1,7 @@
 <script module>
-	import { cubicOut } from 'svelte/easing';
-	import gsap from 'gsap';
 	import { defineMeta } from '@storybook/addon-svelte-csf';
 	import { Sidebar as Sidebar_ } from '.';
 	import Root from '$svelte-atoms/core/components/root/root.svelte';
-	import { on } from '$lib/attachments';
-	import { SidebarBond } from './bond.svelte';
 
 	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 	const { Story } = defineMeta({
@@ -22,33 +18,28 @@
 
 <script lang="ts">
 	let isOpen = $state(false);
-	$inspect(isOpen);
 </script>
 
 <Story name="Sidebar" args={{}}>
 	<Root class="">
 		<Sidebar_.Root class="" bind:open={isOpen}>
-			<div class="flex size-full">
-				<Sidebar_.Content
-					class="flex flex-col border-r px-6 py-10 whitespace-nowrap"
-					initial={(node) => gsap.set(node, { width: isOpen ? 240 : 96 })}
-					animate={(node) =>
-						gsap.to(node, { width: isOpen ? 240 : 96, duration: 0.2, ease: cubicOut })}
-				>
-					<div>
-						<button
-							{@attach (node) => {
-								const bond = SidebarBond.get();
-								return on('click', (ev) => {
-									bond?.state.toggle?.();
-								})(node);
-							}}>Open</button
-						>
-					</div>
-				</Sidebar_.Content>
+			{#snippet children({ sidebar })}
+				<div class="flex size-full">
+					<Sidebar_.Content
+						class="flex max-w-[300px] min-w-32 flex-col border-r px-6 py-10 whitespace-nowrap"
+					>
+						<div>
+							<button
+								onclick={() => {
+									sidebar?.state.toggle?.();
+								}}>Open</button
+							>
+						</div>
+					</Sidebar_.Content>
 
-				<main class="bg-foreground/2 flex-1 p-8">Hello World!</main>
-			</div>
+					<main class="bg-foreground/2 flex-1 p-8">Hello World!</main>
+				</div>
+			{/snippet}
 		</Sidebar_.Root>
 	</Root>
 </Story>
