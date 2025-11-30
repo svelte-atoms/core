98c87e55f7048b8e8d08f3d5a830b193ce443f72
abdel <rioman19@gmail.com>
2025-11-30 23:42:48 +0100

    fix: refactor drawer content animation logic for improved clarity and functionality

--- src/lib/components/drawer/drawer-content.svelte
+++ src/lib/components/drawer/drawer-content.svelte
@@ -3,20 +3,22 @@
 	import { HtmlAtom, type Base } from '$svelte-atoms/core/components/atom';
 	import type { SlideoverContentProps } from './types';
 	import { DrawerBond } from './bond.svelte';
+	import { animate as motion } from 'motion';
 
 	type Element = HTMLElementTagNameMap[E];
 
 	const bond = DrawerBond.get();
+	const isOpen = $derived(bond?.state.props.open);
 
 	let {
 		class: klass = '',
 		children = undefined,
 		onmount = undefined,
 		ondestroy = undefined,
-		animate = undefined,
+		animate = _animate,
 		enter = undefined,
 		exit = undefined,
-		initial = undefined,
+		initial = _initial,
 		...restProps
 	}: SlideoverContentProps<E, B> & HTMLAttributes<Element> = $props();
 
@@ -24,11 +26,24 @@
 		...bond?.content(),
 		...restProps
 	});
+
+	function _initial(node: HTMLElement) {
+		motion(node, { x: isOpen ? 0 : -100+'%', left: 0 }, { duration: 0.3, ease: 'anticipate' });
+	}
+
+	function _animate(node: HTMLElement) {
+		motion(node, { x: isOpen ? 0 : -100+'%', left: 0 }, { duration: 0.3, ease: 'anticipate' });
+	}
 </script>
 
 <HtmlAtom
 	preset="drawer.content"
-	class={['bg-card text-foreground border-border absolute', '$preset', klass]}
+	class={[
+		'bg-card text-foreground border-border pointer-events-none absolute',
+		isOpen && 'pointer-events-auto',
+		'$preset',
+		klass
+	]}
 	{bond}
 	onmount={onmount?.bind(bond.state)}
 	ondestroy={ondestroy?.bind(bond.state)}
