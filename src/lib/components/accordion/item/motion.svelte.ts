9eaff4c5b9ee16417f69368dc7e44337e95f8b56
abdel <rioman19@gmail.com>
2025-12-10 18:07:06 -0500

    adjust duration for enter/exit transition hooks

--- src/lib/components/accordion/item/motion.svelte.ts
+++ src/lib/components/accordion/item/motion.svelte.ts
@@ -9,7 +9,7 @@ export type EnterAccordionItemBodyParams = {
 
 export function enterAccordionItemBody(params: EnterAccordionItemBodyParams = {}) {
 	return (node: HTMLElement) => {
-		const { duration = DURATION.smooth / 1000, delay = 0, ease = 'linear' } = params;
+		const { duration = DURATION.normal / 1000, delay = 0, ease = 'linear' } = params;
 
 		animate(
 			node,
@@ -32,7 +32,7 @@ export type ExitAccordionItemBodyParams = EnterAccordionItemBodyParams;
 
 export function exitAccordionItemBody(params: ExitAccordionItemBodyParams = {}) {
 	return (node: HTMLElement) => {
-		const { duration = DURATION.smooth / 1000, delay = 0.1, ease = 'linear' } = params;
+		const { duration = DURATION.normal / 1000, delay = 0.1, ease = 'linear' } = params;
 
 		animate(
 			node,
