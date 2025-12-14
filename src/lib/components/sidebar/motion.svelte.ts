56315695648ffc278de64c64b983d42516f544ef
abdel <rioman19@gmail.com>
2025-12-13 22:54:42 -0500

    feat: add animateSidebarContent function for sidebar animations

---
+++ src/lib/components/sidebar/motion.svelte.ts
@@ -0,0 +1,40 @@
+import { animate, type Easing } from 'motion';
+import { DURATION } from '$svelte-atoms/core/shared';
+import { SidebarBond } from '.';
+
+type AnimateSidebarContentParams = {
+	duration?: number;
+	delay?: number;
+	ease?: Easing | Easing[];
+	axis?: 'x' | 'y';
+	0?: number | string;
+	1?: number | string;
+};
+
+export function animateSidebarContent(params: AnimateSidebarContentParams) {
+	const {
+		duration = DURATION.fast / 1000,
+		delay = 0,
+		ease = 'easeInOut',
+		axis = 'x',
+		'0': collapsedSize = '96px',
+		'1': expandedSize = 'auto'
+	} = params;
+
+	const bond = SidebarBond.get();
+	const isOpen = bond?.state.props.open ?? false;
+
+	const collapsedProp = axis === 'x' ? 'min-width' : 'min-height';
+	const prop = axis === 'x' ? 'width' : 'height';
+
+	return (node: HTMLElement) => {
+		animate(
+			node,
+			{
+				[prop]: isOpen ? expandedSize : collapsedSize,
+				[collapsedProp]: collapsedSize
+			},
+			{ duration, ease, delay }
+		);
+	};
+}
