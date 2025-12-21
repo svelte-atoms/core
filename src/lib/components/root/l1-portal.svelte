2d225189162070a28f8a5dfd1f615c0d3af7a15d
abdel <rioman19@gmail.com>
2025-12-21 17:30:28 -0500

    fix: add missing portal logic in l1-portal and adjust z-index in toasts-portal

---
+++ src/lib/components/root/l1-portal.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { Portal } from '../portal';
+</script>
+
+<Portal.Outer id="root.l1" class="pointer-events-none absolute inset-0 z-12 overflow-hidden">
+	<Portal.Inner />
+</Portal.Outer>
--- src/lib/components/root/toasts-portal.svelte
+++ src/lib/components/root/toasts-portal.svelte
@@ -2,6 +2,6 @@
 	import { Portal } from '../portal';
 </script>
 
-<Portal.Outer id="root.l3" class="pointer-events-none absolute inset-0 z-16 overflow-hidden">
+<Portal.Outer id="root.l3" class="pointer-events-none absolute inset-0 z-14 overflow-hidden">
 	<Portal.Inner />
 </Portal.Outer>
