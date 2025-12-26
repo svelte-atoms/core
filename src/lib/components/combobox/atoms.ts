682b467c750745ef914084edf7a48eaeb6585339
abdel <rioman19@gmail.com>
2025-12-26 11:17:08 -0500

    rename menu, dropdown, combobox list to content for consistency - prepare .List for removal

--- src/lib/components/combobox/atoms.ts
+++ src/lib/components/combobox/atoms.ts
@@ -1,16 +1,17 @@
 export { default as Root } from './combobox-root.svelte';
 export { default as Trigger } from './combobox-trigger.svelte';
-export { default as Item } from './compobox-item.svelte';
+export { default as Item } from './combobox-item.svelte';
 /**
- * @deprecated use Combobox.Control instead
+ * @deprecated Use Combobox.Control instead
  */
 export { default as Input } from './combobox-control.svelte';
 export { default as Control } from './combobox-control.svelte';
 export {
 	Arrow,
 	Indicator,
+	Content,
 	List,
 	Group,
 	Divider,
 	Title
-} from '$svelte-atoms/core/components/dropdown/atoms';
+} from '../dropdown/atoms';
