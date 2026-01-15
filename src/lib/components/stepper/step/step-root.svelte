94a6b45f57cd6d42cc5b841e75f9adc8b6392511
abdel <rioman19@gmail.com>
2026-01-14 20:52:20 -0500

    fix: remove unused header, body, and error properties

--- src/lib/components/stepper/step/step-root.svelte
+++ src/lib/components/stepper/step/step-root.svelte
@@ -1,42 +1,32 @@
 <script lang="ts" generics="E extends keyof HTMLElementTagNameMap = 'div', B extends Base = Base">
 	import { defineProperty, defineState } from '$svelte-atoms/core/utils';
-	import { HtmlAtom as Atom, type Base } from '$svelte-atoms/core/components/atom';
+	import { type Base } from '$svelte-atoms/core/components/atom';
 	import { StepBond, StepBondState, type StepBondProps } from './bond.svelte';
 	import type { StepRootProps } from './types';
 	import type { Factory } from '$svelte-atoms/core/types';
+	import { onDestroy } from 'svelte';
 
 	let {
 		index,
-		header,
-		body = undefined,
 		disabled = false,
 		completed = false,
 		optional = false,
-		error = false,
-		class: klass = '',
 		children = undefined,
 		factory = _factory as Factory<StepBond>,
-		preset = 'stepper.step' as const,
-		...restProps
 	}: StepRootProps<E, B> = $props();
 
 	const stepProps = defineState<StepBondProps>([
 		defineProperty('index', () => index),
-		defineProperty('header', () => header),
-		defineProperty('body', () => body),
 		defineProperty('disabled', () => disabled),
 		defineProperty('completed', () => completed),
 		defineProperty('optional', () => optional),
-		defineProperty('error', () => error)
 	]);
 
 	const bond = $derived(factory(stepProps).share());
 
 	bond.state.mount();
-
-	const rootProps = $derived({
-		...bond.root(),
-		...restProps
+	onDestroy(()=> {
+		bond.state.unmount();  
 	});
 
 	function _factory(props = stepProps) {
