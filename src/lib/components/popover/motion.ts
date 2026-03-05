66909151c2f66c902f6616f97009bcb16f6033c0
ryu-man <rioman19@gmail.com>
2026-03-04 20:49:45 -0500

    refactor(popover): improve animation parameters and logic

--- src/lib/components/popover/motion.ts
+++ src/lib/components/popover/motion.ts
@@ -1,4 +1,4 @@
-import { animate } from 'motion';
+import { animate, circOut } from 'motion';
 import { PopoverBond } from '.';
 import { DURATION } from '$svelte-atoms/core/shared';
 
@@ -12,7 +12,7 @@ export function animatePopoverContent(params: AnimatePopoverContentParams = {}) 
 	return (node: HTMLElement) => {
 		const bond = PopoverBond.get();
 
-		const { duration = DURATION.quick / 1000, delay = 0, ease = 'easeInOut' } = params;
+		const { duration = DURATION.fast / 1000, delay = 0, ease = circOut } = params;
 
 		const isOpen = bond?.state.props.open ?? false;
 
@@ -22,62 +22,60 @@ export function animatePopoverContent(params: AnimatePopoverContentParams = {}) 
 			return;
 		}
 
+		node.style.transform = '';
+
+		const triggerElement = bond.elements.trigger;
+
+		if (!triggerElement) {
+			return;
+		}
+
+		const triggerRect = triggerElement.getBoundingClientRect();
+		const nodeRect = node.getBoundingClientRect();
+
+		const scaleX = triggerRect.width / nodeRect.width;
+		const scaleY = triggerRect.height / nodeRect.height;
+
+		const x = triggerRect.x - nodeRect.x;
+		const y = triggerRect.y - nodeRect.y;
+
 		const placement = position.placement;
 
 		const dy = placement?.startsWith('top') ? -1 : placement?.startsWith('bottom') ? 1 : 0;
 		const dx = placement?.startsWith('left') ? -1 : placement?.startsWith('right') ? 1 : 0;
 
-		const offset = bond.state.props.offset;
 
-		const xOffset = dx * offset;
-		const yOffset = dy * offset;
 
 		const openAsNumber = +isOpen;
 
-		const arrowClientWidth = bond?.elements.arrow?.clientWidth ?? 0;
-		const arrowClientHeight = bond?.elements.arrow?.clientHeight ?? 0;
 
-		const getTransformOrigin = () => {
-			switch (placement) {
-				case 'top':
-				case 'top-start':
-				case 'top-end':
-					return 'bottom';
-				case 'bottom':
-				case 'bottom-start':
-				case 'bottom-end':
-					return 'top';
-				case 'left':
-				case 'left-start':
-				case 'left-end':
-					return 'right';
-				case 'right':
-				case 'right-start':
-				case 'right-end':
-					return 'left';
-				default:
-					return 'center';
-			}
-		};
+		// const transformOriginX = dx >= 0 ? 'left' : 'right';
+		const transformOriginY = dy >= 0 ? 'top' : 'bottom';
 
-		const transformOrigin = getTransformOrigin();
+		node.style.transformOrigin = `center ${transformOriginY}`;
 
-		const s0 = 0.9;
-		const s1 = 1;
+		node.style.pointerEvents = 'none';
 
-		const from = isOpen ? s1 : s0;
-
-		animate(
+		const c = animate(
 			node,
 			{
-				opacity: openAsNumber,
-				y: dy * (!isOpen ? -1 : 0) * (arrowClientHeight + yOffset),
-				x: dx * (!isOpen ? -1 : 0) * (arrowClientWidth + xOffset),
-				scaleY: dy ? (isOpen ? [from, s1] : [s1, from]) : undefined,
-				scaleX: dx ? (isOpen ? [from, s1] : [s1, from]) : undefined,
-				transformOrigin
+				scaleX: isOpen ? [0, 1] : [1, 0],
+				scaleY: isOpen ? [scaleY, 1] : [1, scaleY],
+				translateX: isOpen ? [`${x}px`, '0px'] : ['0px', `${x}px`],
+				translateY: isOpen ? [`${y}px`, '0px'] : ['0px', `${y}px`],
+				opacity: openAsNumber
 			},
-			{ duration, delay, ease }
+			{
+				duration,
+				easing: ease,
+				delay
+			}
 		);
+
+		c.then(() => {
+			node.style.pointerEvents = '';
+		});
+
+		// resolve({ duration, delay, controller: c });
 	};
 }
