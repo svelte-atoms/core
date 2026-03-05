b88461318f191ca85317320d15b6c7deeed7d3fe
ryu-man <rioman19@gmail.com>
2026-03-04 20:47:14 -0500

    refactor(port): improve opacity handling during mount

--- src/lib/components/portal/utils.ts
+++ src/lib/components/portal/utils.ts
@@ -3,20 +3,21 @@ export function port(node: HTMLElement, target: HTMLElement = document.body) {
 		throw Error('[actions] portal: Target element is undefined !');
 	}
 
+	const opacity = node.style.opacity;
+
 	if (node.parentElement !== target) {
-		node.hidden = true;
+		node.style.opacity = '0';
 
 		// Check if element is already mounted on target
 		if (node.parentElement !== target) {
 			target.appendChild(node);
 			requestAnimationFrame(() => {
-				node.hidden = false;
+				node.style.opacity = opacity;
 			});
 		}
 	}
 
 	return () => {
-		node.hidden = true;
 		node.remove();
 	};
 }
