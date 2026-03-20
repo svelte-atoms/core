ff44e506be84bf7f15f250c70b0f173ca60a3e8e
ryu-man <rioman19@gmail.com>
2026-03-19 21:33:31 -0400

    refactor(props-table): simplify row rendering logic

--- src/docs/md/components/props-table.svelte
+++ src/docs/md/components/props-table.svelte
@@ -6,9 +6,17 @@
 	}
 
 	let { props }: Props = $props();
+
+	function row(item: PropDefinition) {
+		return `| **${item.name}** | \`${item.type}\` | \`${item.default}\` | ${item.description} |`;
+	}
+
+	function rows(items: PropDefinition[]) {
+		return items.map(row).join('\n');
+	}
 </script>
+<!-- eslint-disable-next-line svelte/no-useless-mustaches -->
+{"\n"}
 | name | type | default | description |
 | --- | --- | --- | --- |
-{#each props as prop (prop.name)}
-| {prop.name} | `{prop.type}` | `{prop.default}` | {prop.description} |
-{/each}
+{rows(props)}
