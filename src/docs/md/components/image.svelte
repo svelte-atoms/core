f0b597440f6d906368e045cea7e1447a5a0204bd
ryu-man <rioman19@gmail.com>
2026-03-19 21:16:11 -0400

    refactor: remove unused Svelte components and exports

--- src/docs/md/components/image.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	interface Props {
-		src: string;
-		alt?: string;
-	}
-
-	let { src, alt = '' }: Props = $props();
-</script>
-![{alt}]({src})
--- src/docs/md/components/index.ts
+++ src/docs/md/components/index.ts
@@ -1,7 +1,2 @@
-export { default as CodeBlock } from './code-block.svelte';
-export { default as List } from './list.svelte';
-export { default as InlineCode } from './inline-code.svelte';
-export { default as Link } from './link.svelte';
-export { default as Image } from './image.svelte';
 export { default as PropsTable } from './props-table.svelte';
 export { default as FrontMatter } from './front-matter.svelte';
