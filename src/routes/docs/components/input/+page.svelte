0f814abcd7db8539b370361030fd5c0e7ad4af42
ryu-man <rioman19@gmail.com>
2026-03-25 08:56:41 -0400

    enhance input controls with new examples and props

--- src/routes/docs/components/input/+page.svelte
+++ src/routes/docs/components/input/+page.svelte
@@ -2,6 +2,7 @@
   import { metadata } from './shared';
   import Content from './content.svelte';
 </script>
+
 <svelte:head>
   <title>{metadata.title}</title>
   <meta name="description" content={metadata.description} />
